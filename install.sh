428b437914bd7a0f6ebe1f56145e2e6dfb5b6dd4
Domingos Lamas Rodrigues <domingoslamas@gmail.com>
2026-05-31 14:17:45 +0100

    Integrated motors_sync

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 repo=klipper_addon_de
 repo_path="$(cd "$(dirname "$0")" && pwd)"
-beacon_path=$repo_path"/beacon"
+
 
 # Exit if root
 if [ "$(id -u)" = "0" ]; then
@@ -17,11 +17,14 @@ module_name4="beacon_adaptive_heat_soak.py"
 module_name5="beacon_adaptive_heat_soak_model_training.csv"
 module_name6="beacon_true_zero_correction.py"
 beacon_name="beacon.py"
+motorsync_name="motors_sync.py"
 
 klipper_path="$HOME/klipper"
 klipper_env_path="$HOME/klippy-env"
 extras_path=${klipper_path}/klippy/extras/
 kinematics_path=${klipper_path}/klippy/kinematics/
+beacon_path=$repo_path"/beacon"
+motorsync_path=$repo_path"/motors-sync"
 
 # Linking
 ln -sf "$repo_path/$module_name1" "${kinematics_path}${module_name1}"
@@ -31,6 +34,7 @@ ln -sf "$repo_path/$module_name4" "${extras_path}${module_name4}"
 ln -sf "$repo_path/$module_name5" "${extras_path}${module_name5}"
 ln -sf "$repo_path/$module_name6" "${extras_path}${module_name6}"
 ln -sf "$beacon_path/$beacon_name" "${extras_path}${beacon_name}"
+ln -sf "$motorsync_path/$motorsync_name" "${extras_path}${motorsync_name}"
 
 # Add to .git/info/exclude if not already present
 exclude_file="${klipper_path}/.git/info/exclude"
@@ -41,6 +45,7 @@ exclude_entry4="klippy/extras/${module_name4}"
 exclude_entry5="klippy/extras/${module_name5}"
 exclude_entry6="klippy/extras/${module_name6}"
 exclude_beacon="klippy/extras/${beacon_name}"
+exclude_motorsync="klippy/extras/${motorsync_name}"
 if ! grep -qxF "$exclude_entry1" "$exclude_file"; then
     echo "$exclude_entry1" >> "$exclude_file"
 fi
@@ -61,6 +66,9 @@ if ! grep -qxF "$exclude_entry6" "$exclude_file"; then
 fi
 if ! grep -qxF "$exclude_beacon" "$exclude_file"; then
     echo "$exclude_beacon" >> "$exclude_file"
+fi
+if ! grep -qxF "$exclude_motorsync" "$exclude_file"; then
+    echo "$exclude_motorsync" >> "$exclude_file"
 fi
 
 blk_path=~/printer_data/config/moonraker.conf
@@ -90,8 +98,15 @@ fi
 
 sudo apt update
 
+
+echo "Adaptive Heatsoak: installing python requirements to env."
+"${klipper_env_path}/bin/pip" install pygam
+
+echo "Motor Sync: installing requirements."
+sudo apt install libatlas-base-dev libopenblas-dev
+
 # install beacon requirements to env
-echo "beacon: installing python requirements to env, this may take 10+ minutes."
+echo "beacon: installing python requirements to env."
 "${klipper_env_path}/bin/pip" install -r "${beacon_path}/requirements.txt"
 echo "beacon: Updating firmware."
 "$klipper_env_path/bin/python" "$beacon_path/update_firmware.py" update all
